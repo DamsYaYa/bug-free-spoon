a30ecef8b410399f52edb2d64a2f4e66cb7f3b82
Екатерина <ekaterinadams@gmail.com>
2018-02-11 21:59:34 +0300

    Оптимизация переходов между страницами

--- addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
+++ addressbook-web-tests/addressbook-web-tests/appmanager/NavigationHelper.cs
@@ -21,17 +21,31 @@ namespace WebAddressbookTests
         public void OpenHomePage()
         {
             // Открытие начальной страницы
+            if(driver.Url == baseURL + "/addressbook/")
+            {
+                return;
+            }
             driver.Navigate().GoToUrl(baseURL + "addressbook/");
         }
         public void OpenGroupPage()
         {
             // Переход на страницу со списком групп
+            if(driver.Url == baseURL + "/addressbook/group.php"
+                && IsElementPresent(By.Name("new")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("groups")).Click();
         }
 
         public void GoToHomePage()
         {
             // Переход на страницу со списком контактов
+            if (driver.Url == baseURL + "/addressbook/"
+                 && IsElementPresent(By.XPath("//input[@value='Send e-Mail']")))
+            {
+                return;
+            }
             driver.FindElement(By.LinkText("home")).Click();
         }
     }
